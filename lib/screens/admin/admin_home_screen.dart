79f5ca11d7737fc2257b1ed98d62821b77867f49
AronBks <aroncotp@gmail.com>
2026-03-23 16:00:38 -0400

    Renombrar acción de cierre de sesión a 'Salir'

--- lib/screens/admin/admin_home_screen.dart
+++ lib/screens/admin/admin_home_screen.dart
@@ -388,7 +388,7 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
       builder: (BuildContext ctx) {
         return AlertDialog(
           icon: Icon(Icons.logout, color: colorScheme.error, size: 32),
-          title: const Text('Cerrar Sesión'),
+          title: const Text('Salir'),
           content: const Text('¿Estás seguro que deseas cerrar sesión?'),
           actions: [
             TextButton(
@@ -398,7 +398,7 @@ class _AdminHomeScreenState extends State<AdminHomeScreen> {
             FilledButton(
               onPressed: () => Navigator.pop(ctx, true),
               style: FilledButton.styleFrom(backgroundColor: colorScheme.error),
-              child: const Text('Cerrar Sesión'),
+              child: const Text('Salir'),
             ),
           ],
         );
